7dc0b4e491d3407bf5ac31d65a4d2f20e188086f
Mario <mariovalladares178@gmail.com>
2026-04-03 12:18:39 -0600

    commit2

--- set-env.js
+++ set-env.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 
-// Ruta donde se creará tu archivo de entorno de producción
 const targetPath = './src/environments/environment.ts';
 
-const apirUrl = process.env.apirUrl || 'http://localhost:8084';
-const keycloakUrl = process.env.keycloakUrl || 'http://localhost:8084';
-const keycloakRealm = process.env.keycloakRealm || 'http://localhost:8084';
-const keycloakClient = process.env.keycloakClient || 'http://localhost:8084';
+// Usamos el estándar MAYÚSCULAS para leer del servidor
+// Y le ponemos valores por defecto lógicos para tu entorno local
+const apiUrl = process.env.API_URL || 'http://localhost:8084';
+const keycloakUrl = process.env.KEYCLOAK_URL || 'http://localhost:8180';
+const keycloakRealm = process.env.KEYCLOAK_REALM || 'seguridad-app';
+const keycloakClient = process.env.KEYCLOAK_CLIENT || 'front-app2';
 
-// Construimos el contenido del archivo exactamente como lo usaría Angular
 const envConfigFile = `
 export const environment = {
   production: true,
-  apirUrl: '${apirUrl}',
+  apiUrl: '${apiUrl}',
   keycloakUrl: '${keycloakUrl}',
-  keycloakRealm : '${keycloakRealm}',
-  keycloakClient: '${keycloakClient}',
+  keycloakRealm: '${keycloakRealm}',
+  keycloakClient: '${keycloakClient}'
 };
 `;
 
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
-export const environment={
-  production: true,
+export const environment = {
+  production: true, // En tu local debe decir false
   apiUrl: 'http://localhost:8084',
-  keycloakUrl: 'keycloak',
-  keycloakRealm : 'keycloak-realm',
-  keycloakClient: 'keycloak-client',
-}
+  keycloakUrl: 'http://localhost:8180', // Asumiendo que tu Keycloak local está en el 8180
+  keycloakRealm: 'seguridad-app',
+  keycloakClient: 'front-app2'
+};
